30c827d820b8ecf92ffb986fb59dd05b0238d398
keijidosha <okumura@Keis-Air.local>
2018-02-18 12:03:24 +0900

    HTTPヘッダーを表示

--- client/cio/src/main/kotlin/HttpClientCIO.kt
+++ client/cio/src/main/kotlin/HttpClientCIO.kt
@@ -1,13 +1,27 @@
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.*
+import io.ktor.client.response.*
 import kotlinx.coroutines.experimental.*
+
+import io.ktor.client.features.cookies.*
+//import io.ktor.http.*
 
 fun main( args: Array<String> ) {
     val client = HttpClient( CIO )
 
     val response = runBlocking {
-        client.get<String>( host = "www.yahoo.co.jp" )
+        val res = client.get<HttpResponse>( host = "www.yahoo.co.jp" )
+        client.cookies( "www.yahoo.co.jp" ).forEach { k, v ->
+            println( "${k}: ${v}" )
+        }
+        println( res.version )
+        println( res.status )
+        res.headers.forEach{ k, v ->
+            println( "$k: $v" )
+        }
+        println()
+        res.readText()
     }
 
     println( response )
