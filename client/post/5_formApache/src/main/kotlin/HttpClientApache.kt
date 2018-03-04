509a1a4a28d99ac1d9f289d4f9f5959a44843203
keijidosha <>
2018-03-04 17:26:23 +0900

    Apacheを使ったPOSTフォームのサンプル

---
+++ client/post/5_formApache/src/main/kotlin/HttpClientApache.kt
@@ -0,0 +1,37 @@
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.post
+import kotlinx.coroutines.experimental.runBlocking
+import io.ktor.http.HeadersBuilder
+
+fun main( args: Array<String> ) {
+    /*
+    val httpRequestBuilder1: HttpRequestBuilder.() -> Unit = {
+        val headersBuilder: HeadersBuilder.() -> Unit = {
+            append( "Content-Type", "application/x-www-form-urlencoded" )
+        }
+        headers( headersBuilder )
+    }
+    */
+    /*
+    val httpRequestBuilder2: HttpRequestBuilder.() -> Unit = {
+        headers{
+            append( "Content-Type", "application/x-www-form-urlencoded" )
+        }
+    }
+    */
+    HttpClient( Apache ).use {
+        val response = runBlocking {
+            it.post<String>( scheme = "http", host = "localhost", path = "/postform.php", port = 8000, body = "data=hoge"
+                , block = {
+                    headers{
+                        append( "Content-Type", "application/x-www-form-urlencoded" )
+                    }
+                }
+                //body = "data=hoge", block = httpRequestBuilder2 )
+            )
+        }
+
+        println( response )
+    }
+}
